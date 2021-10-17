148e2f8e491df4dda6e02e2b96d86135423d7ad5
lekoal <coreglus@gmail.com>
2021-10-17 19:40:57 +0300

    Note class is added

--- app/src/main/java/com/example/mynotesapp/Note.java
+++ app/src/main/java/com/example/mynotesapp/Note.java
@@ -1,5 +1,42 @@
 package com.example.mynotesapp;
 
-public class Note {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class Note {
+    private String title;
+    private String content;
+    private String date;
+    private String time;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy", Locale.US);
+        SimpleDateFormat formatForTimeNow = new SimpleDateFormat("hh:mm:ss", Locale.US);
+        this.date = formatForDateNow.format(dateNow);
+        this.time = formatForTimeNow.format(dateNow);
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
