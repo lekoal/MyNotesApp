c01e9ef3f7fbf616a7fa2444f90118885cc59c8a
lekoal <coreglus@gmail.com>
2021-10-17 20:50:51 +0300

    First fragment in progress

--- app/src/main/java/com/example/mynotesapp/NotesFragment.java
+++ app/src/main/java/com/example/mynotesapp/NotesFragment.java
@@ -10,26 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NotesFragment extends Fragment {
 
     public NotesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NotesFragment newInstance(String param1, String param2) {
         NotesFragment fragment = new NotesFragment();
         Bundle args = new Bundle();
@@ -37,14 +23,6 @@ public class NotesFragment extends Fragment {
         args.putString("ARG_PARAM2", param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
     }
 
     @Override
